b74906e3d93f933f8c6c068d75420fa88c33ae57
jo920208 <jo920208@gmail.com>
2020-01-11 09:40:39 +0900

    Modify register container content state init

--- client/containers/register-container.tsx
+++ client/containers/register-container.tsx
@@ -40,7 +40,7 @@ const RegisterDiv = styled.div`
 const RegisterContainer: React.FC = () => {
   const dispatch = useDispatch();
 
-  const [content, setContent] = useState<string>('input content');
+  const [content, setContent] = useState<string>('');
 
   const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => setContent(e.currentTarget.value);
 
